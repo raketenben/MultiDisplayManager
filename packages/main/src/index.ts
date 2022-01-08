ad4a5d091752706802ca1828998f819d1cd71a36
Ben Ehrengruber <35403861+raketenben@users.noreply.github.com>
2022-01-08 18:40:22 +0100

    changed update behaviour on kiosk mode clients

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -90,8 +90,17 @@ if (import.meta.env.MODE === 'development') {
 
 // Auto-updates
 if (import.meta.env.PROD) {
-  autoUpdater.autoDownload = true;
-  autoUpdater.checkForUpdatesAndNotify();
+  if(!process.argv.includes('--viewer')){
+    autoUpdater.checkForUpdatesAndNotify();
+  }else{
+    autoUpdater.autoDownload = true;
+    autoUpdater.checkForUpdates();
+    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+      console.log('Update downloaded');
+      console.log(releaseNotes,releaseName);
+      autoUpdater.quitAndInstall();
+    });
+  }
 } 
 
 //error logging
